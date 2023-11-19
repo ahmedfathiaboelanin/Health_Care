4ce7388a38e85260113e4f97fa822a73a23eeeda
Ahmed <ahmedfathiaboelanin@gmail.com>
2023-11-19 22:49:21 +0200

    create recommendition folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter,Route,Routes } from 'react-router-dom'
-import Recommendation from './Pages/Recommendation/Recommendation'
+import Diagnosis from './Pages/Services/Diagnosis/Diagnosis'
 function App() {
   
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/recommendation" element={<Recommendation />} />
+          <Route path='/services'>
+            <Route index element={<h1>All Services</h1>}/>
+            <Route path='diagnosis' element={<Diagnosis/>}/>
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
--- src/Pages/Recommendation/Recommendation.jsx
+++
@@ -1,8 +0,0 @@
-import "./Recommendation.css"
-function Recommendation() {
-  return (
-    <h1>Recommendation</h1>
-  )
-}
-
-export default Recommendation
