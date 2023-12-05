c0029377419b9883bbb76d8991c43272e85d9085
Ahmed <ahmedfathiaboelanin@gmail.com>
2023-12-05 13:08:15 +0200

    add footer to all pages

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Select from './Components/SelectDropDownMenu/Select'
 import { BrowserRouter,Route,Routes } from 'react-router-dom'
 import Diagnosis from './Pages/Services/Diagnosis/Diagnosis'
 import NavBar from './Components/NavBar/NavBar'
+import Footer from './Components/Footer/Footer'
 function App() {
   
   return (
@@ -20,6 +21,7 @@ function App() {
             <Route path='diagnosis' element={<Diagnosis/>}/>
           </Route>
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </>
   )
